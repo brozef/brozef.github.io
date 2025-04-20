31e29d096a1565c19357e6bbca27b0c0ac91666f
Joe Dowling <10679867+brozef@users.noreply.github.com>
2025-04-20 20:52:55 +1000

    output

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,13 @@
 import Container from "@/app/_components/container";
 import { Intro } from "@/app/_components/intro";
-import { MoreStories } from "@/app/_components/more-stories";
-import { getAllPosts } from "@/lib/api";
 import { Hero } from "@/app/_components/hero";
 
 export default function Index() {
-  const allPosts = getAllPosts();
 
   return (
     <main className="flex flex-col min-h-screen">
       <Intro />
       <Hero />
-      {allPosts.length > 0 && <MoreStories posts={allPosts} />}
     </main>
   );
 }
