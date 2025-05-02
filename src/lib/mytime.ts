4fd409aace69ef9df06219faa6f4a7b82a500c44
Brozef <10679867+brozef@users.noreply.github.com>
2025-05-02 14:23:40 +1000

    use timezone name rather than hour offset for build machine

--- src/lib/mytime.ts
+++ src/lib/mytime.ts
@@ -1,4 +1,4 @@
-const options:Intl.DateTimeFormatOptions = { weekday: 'short', timeZone:'+10', hour12: true, hour: 'numeric', minute: 'numeric'};
+const options:Intl.DateTimeFormatOptions = { weekday: 'short', timeZone:'Australia/Brisbane', hour12: true, hour: 'numeric', minute: 'numeric'};
 const formatter:Intl.DateTimeFormat = new Intl.DateTimeFormat('en-GB', options);
 
 export function getMyCurrentTime(): string {
