c95796b2b0dcfaa74c1fe7621c60a394a76e8b07
Joe Dowling <10679867+brozef@users.noreply.github.com>
2025-05-05 14:10:42 +1000

    adjust time text responsive behaviour

--- src/app/_components/intro.tsx
+++ src/app/_components/intro.tsx
@@ -14,9 +14,9 @@ export function Intro() {
     }, [timeStr]);
 
   return (
-    <section className="flex-row flex justify-between items-end mt-16 mb-2 md:mb-2 bg-white">
-      <p className="text-right text-[11px] pl-3 md:pl-10 mb-3 lg:mb-4">
-       Brisbane, Aus | {timeStr}
+    <section className="flex-row flex justify-between items-end mt-10 md:mt-16 mb-2 bg-white">
+      <p className="text-justify [text-align-last:justify] text-[11px] pl-3 md:pl-10 mb-3 lg:mb-4">
+       Brisbane, Aus <span className="hidden md:inline">|</span> <br className="inline md:hidden"/>{timeStr}
       </p>
       <h1 className="text-right text-7xl md:text-9xl font-bold tracking-tighter leading-tight pr-2 md:pr-8">
         Brozef
