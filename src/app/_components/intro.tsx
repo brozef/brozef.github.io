43a9aef9bfa83d12caf2df0b2a7b93a5dfcbc0f7
Brozef <10679867+brozef@users.noreply.github.com>
2025-05-25 21:14:36 +1000

    add initial avatar

--- src/app/_components/intro.tsx
+++ src/app/_components/intro.tsx
@@ -1,24 +1,27 @@
 'use client'
-import { getMyCurrentTime } from "@/lib/mytime";
+import { getMyCurrentTime, TimeInfo } from "@/lib/mytime";
 import { useEffect, useState } from "react";
 
 export function Intro() {
-  const [ timeStr, setTimeStr ] = useState<string>(getMyCurrentTime());
+  const [ timeInfo, setTimeInfo ] = useState<TimeInfo>(getMyCurrentTime());
   
     useEffect(() => {
       const seconds = 60 - new Date().getSeconds();
       const timer = setTimeout(() => {
-        setTimeStr(getMyCurrentTime());
+        setTimeInfo(getMyCurrentTime());
       }, seconds * 1000);
 
       return () => clearTimeout(timer);
-    }, [timeStr]);
+    }, [timeInfo]);
 
   return (
     <section className="flex-row flex justify-between items-end mt-10 md:mt-16 mb-2 bg-white">
-      <p className="text-justify [text-align-last:justify] text-[11px] pl-3 md:pl-10 mb-3 lg:mb-4">
-       Brisbane, Aus <span className="hidden md:inline">|</span> <br className="inline md:hidden"/>{timeStr}
-      </p>
+      <div className="flex items-center pl-3 md:pl-10 mb-3 lg:mb-4 gap-2">
+        <img src={timeInfo.activity} width="32" height="32"/>
+        <p className="text-justify [text-align-last:justify] text-[11px] leading-[16px] md:leading-[32px]">
+        Brisbane, Aus <span className="hidden md:inline">|</span> <br className="inline md:hidden"/>{timeInfo.time}
+        </p>
+      </div>
       <h1 className="text-right text-7xl md:text-9xl font-bold tracking-tighter leading-tight pr-2 md:pr-8">
         Brozef
       </h1>
--- src/lib/mytime.ts
+++ src/lib/mytime.ts
@@ -1,6 +1,35 @@
 const options:Intl.DateTimeFormatOptions = { weekday: 'short', timeZone:'Australia/Brisbane', hour12: true, hour: 'numeric', minute: 'numeric'};
 const formatter:Intl.DateTimeFormat = new Intl.DateTimeFormat('en-GB', options);
 
-export function getMyCurrentTime(): string {
-   return formatter.format(new Date());
+export type TimeInfo = {
+  time: string;
+  activity: string;
+};
+
+export function getMyCurrentTime(): TimeInfo {
+   const date = new Date();
+   const day = date.getDay();
+   const hours = date.getHours();
+
+   let activity = "./assets/couch.webp";
+   if (day == 0 || day == 6) {
+      //weekends
+      if (hours > 4 && hours < 12) {
+         activity = "./assets/cycle.webp";
+      } else if (hours > 20 || hours < 5) {
+         activity = "./assets/sleep.webp";
+      }
+   } else {
+      //weekdays
+      if (hours > 9 && hours < 17) {
+         activity = "./assets/office.webp";
+      } else if (hours > 20 || hours < 6) {
+         activity = "./assets/sleep.webp";
+      }
+   }
+
+   return {
+      activity,
+      time: formatter.format(date)
+   };
 }
